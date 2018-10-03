92c4ec1d240567057a60b9dbf99ef19a0c79ac5b
Kenneth Maguire <kennethmaguire@Kenneths-MacBook-Pro.local>
2018-10-03 12:58:37 -0600

    remove punctuation from string

--- Search.py
+++ Search.py
@@ -1,3 +1,7 @@
+import re
+
+import string
+
 filename = "shakespeare.txt"
 f = open(filename, 'r')
 
@@ -5,14 +9,15 @@ f = open(filename, 'r')
 wordList = []
 
 for line in f:
+    line = line.translate(None, string.punctuation) #remove punctuation from input sequence
     for word in line.split():
         wordList.append(word)
 
 i = 0
-while i < 10:
+while i < 100:
     print(wordList[i])
     i += 1
-i = (len(wordList) - 10)
+i = (len(wordList) - 100)
 while i < len(wordList):
     print(wordList[i])
     i += 1
